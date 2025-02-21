d1297d47e99a0634068d5b0c73f9306b5a91aa51
kdesnos <kdesnos@insa-rennes.fr>
2025-02-21 15:07:08 +0100

    Update generation of config file from trace to: - Correct code - Configure tasks to allow up to 3 retries. - Increase target runtime by 25% at each retry.

--- config_file_generator.py
+++ config_file_generator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 def generate_nextflow_config(trace_df, output_config_file):
     """
@@ -18,14 +19,21 @@ def generate_nextflow_config(trace_df, output_config_file):
     # Open the output file for writing
     with open(output_config_file, 'w') as file:
         # Write the process block header
+        file.write(f"// Config file generated on the {datetime.datetime.now()}\n\n")
         file.write("process {\n")
+        file.write("    // Default error handling strategy for all process errors.\n")
+        file.write("    errorStrategy = 'retry'\n")
+        file.write("    maxRetries = 3\n\n")
 
         # Write the time configuration for each process
+        file.write("    // Per-process time limit corresponding to worst case execution time from traces.\n")
+        file.write("    // 60 extra seconds are added to account for the early termination of jobs by Slurm,\n")
+        file.write("    // as requested by Nextflow job with the B:USR2 signal.\n")
         for index, row in max_realtime_df.iterrows():
             process = row['process']
             max_time_seconds = row['realtime'].total_seconds()
             file.write(f"    withName: '{process}' {{\n")
-            file.write(f"        time = {{ ({max_time_seconds + 60}) * Math.pow(1.2, task.attempt)) * 1.s }}\n")
+            file.write(f"        time = {{ ({max_time_seconds} + 60.0) * Math.pow(1.25, (task.attempt - 1)) * 1.s }}\n")
             file.write("    }\n\n")
 
         # Write the process block footer
