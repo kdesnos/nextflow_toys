f6640f3583d1a14d60d145cec7da6b3cfc3b8f38
kdesnos <kdesnos@insa-rennes.fr>
2025-03-23 10:27:20 +1100

    Safer config file gen

--- config_file_generator.py
+++ config_file_generator.py
@@ -4,22 +4,25 @@ import datetime
 
 def generate_nextflow_config(trace_df, output_config_file):
     """
-    Generates a Nextflow configuration file based on the maximum realtime and peak_rss values for each process.
+    Generates a Nextflow configuration file based on the maximum realtime and peak_vmem values for each process.
 
     Parameters:
-    - trace_df (pd.DataFrame): DataFrame containing 'process', 'realtime', and 'peak_rss' columns.
+    - trace_df (pd.DataFrame): DataFrame containing 'process', 'realtime', and 'peak_vmem' columns.
     - output_config_file (str): Path to the output configuration file.
 
     Returns:
     - None: Writes the configuration to the specified file.
     """
+    max_nb_retries = 3
 
-    # Calculate the maximum realtime and peak_rss for each unique process
+    # Calculate the maximum realtime and peak_vmem for each unique process
     max_realtime_df = trace_df.groupby('process')['realtime'].max().reset_index()
-    max_rss_df = trace_df.groupby('process')['peak_rss'].max().reset_index()
+    max_vmem_df = trace_df.groupby('process')['peak_vmem'].max().reset_index()
+    pre_alloc_memory_df = trace_df.groupby('process')['memory'].max().reset_index()
 
     # Merge the two DataFrames on the 'process' column
-    merged_df = pd.merge(max_realtime_df, max_rss_df, on='process')
+    merged_df = pd.merge(max_realtime_df, max_vmem_df, on='process')
+    merged_df = pd.merge(merged_df, pre_alloc_memory_df, on='process')
 
     # Identify processes with at least one None value in the memory column
     processes_with_none_memory = trace_df[trace_df['memory'].isnull()]['process'].unique()
@@ -34,24 +37,33 @@ def generate_nextflow_config(trace_df, output_config_file):
         file.write("process {\n")
         file.write("    // Default error handling strategy for all process errors.\n")
         file.write("    errorStrategy = 'retry'\n")
-        file.write("    maxRetries = 3\n\n")
+        file.write(f"    maxRetries = {max_nb_retries}\n\n")
 
         # Write the time and memory configuration for each process
         file.write("    // Per-process time and memory limits corresponding to worst case execution from traces.\n")
         file.write("    // 60 extra seconds are added to account for the early termination of jobs by Slurm,\n")
         file.write("    // as requested by Nextflow job with the B:USR2 signal.\n")
+        file.write("    // A safety margin of 25% is added to the original memory constraint at first try.\n")
+        file.write("    // Memory constraint is relaxed at each retry until it reach the one successfuly used in traces.\n")
         for index, row in merged_df.iterrows():
             process = row['process']
             max_time_seconds = ceil(row['realtime'].total_seconds())
-            max_rss_mb = ceil(row['peak_rss'] / (1024 * 1024)) if row['peak_rss'] > 0 else 1  # Convert peak_rss to MB
+            max_vmem_mb = ceil(row['peak_vmem'] / (1024 * 1024)) if row['peak_vmem'] > 0 else 1  # Convert peak_vmem to MB
+            successful_mem_mb = ceil(row['memory'] / (1024 * 1024)) if row['memory'] > 0 else 1  # Convert memory to MB
             file.write(f"    withName: '{process}' {{\n")
             file.write(f"        time = {{ ({max_time_seconds} + 60.0) * Math.pow(1.25, (task.attempt - 1)) * 1.s }}\n")
+            # Check if observed mem footprint is greater than the max_vmem_mb. In which case, just increase at each retry.
+            if successful_mem_mb > max_vmem_mb:
+                memory_config = f"memory = {{ ({max_vmem_mb}*1.25 + ({successful_mem_mb} - {max_vmem_mb}*1.25) * task.attempt / {max_nb_retries}) * 1.MB }}"
+            else:
+                memory_config = f"memory = {{ ({max_vmem_mb} * Math.pow(1.25, task.attempt))  * 1.MB }}"
+
             if row['printMemory']:
-                file.write(f"        memory = {max_rss_mb}.MB \n")
+                file.write(f"        {memory_config}\n")
             else:
                 file.write(f"        // Memory config not printed because no memory config was previously give for this process.\n")
                 file.write(f"        // This is most likely the result of an alternative memory config given with clusterOptions.\n")
-                file.write(f"        // memory = {max_rss_mb}.MB \n")
+                file.write(f"        // {memory_config}\n")
             file.write("    }\n\n")
 
         # Write the process block footer
